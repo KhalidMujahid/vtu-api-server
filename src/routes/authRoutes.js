7e29726275982ef50866d20012134756fc19940a
KhalidMujahid <binkhalid267@gmail.com>
2026-03-18 09:48:24 -0700

    web

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const authController = require('../controllers/authController');
+const { protect } = require('../middlewares/auth');
 
 /**
  * @swagger
